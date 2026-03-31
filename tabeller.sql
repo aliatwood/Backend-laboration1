c021f521bee5976b91eec3744ab114cfcc1d84b9
aliatwood <aliatwood0104@gmail.com>
2026-03-31 16:37:44 +0200

    npm init -y och npm install express sqlite3 ejs
    Initierat projekt med npm och installerat dependencies: express, sqlite3 och ejs.

--- tabeller.sql
+++ tabeller.sql
@@ -3,8 +3,8 @@
 /* courses(id(pk), courseCode, courseName, syllabus, progression) */
 create table courses(
     id integer not null,
-    courseCode integer,
-    courseName varchar(30),
-    syllabus varchar(200),
-    progression varchar(10)
+    courseCode varchar(15) not null,
+    courseName varchar(30) not null,
+    syllabus varchar(200) not null,
+    progression varchar(10) not null
 );
