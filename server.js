879eb57482605910938d6fcac7b6768ab36341c6
aliatwood <aliatwood0104@gmail.com>
2026-03-31 19:01:57 +0200

    skapade myDb.db och mina 3 ejs filer

--- server.js
+++ server.js
@@ -2,6 +2,16 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+const sqlite3 = require("sqlite3").verbose();
+const db = new sqlite3.Database("./myDb.db", (err) => {
+    if (err) {
+        console.log(err.message)
+    }
+    else {
+        console.log("Databasen är skapad");
+    }
+})
+
 app.use(express.urlencoded({extended:true}));
 
 app.use(express.static("public"));
@@ -9,8 +19,16 @@ app.use(express.static("public"));
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) =>{
-    res.send("hello world");
+    res.render("index", {courses: [] });
 });
+
+app.get("/add", (req, res) => {
+    res.render("add");
+})
+
+app.get("/about", (req, res) => {
+    res.render("about");
+})
 
 app.listen(port, () =>{
     console.log("servern körs på " + port);
